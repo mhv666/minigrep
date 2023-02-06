6cf4532e60a3929d131d878b692a46e8ecbb2530
mhorrach <marti.horrachvicens@metro-markets.de>
2023-02-06 15:12:25 +0100

    read

--- minigrep/target/debug/deps/minigrep-a96225767c2cdeba.d
+++
@@ -1,5 +0,0 @@
-/home/mhorrach/Repos/minigrep/minigrep/target/debug/deps/minigrep-a96225767c2cdeba: src/main.rs
-
-/home/mhorrach/Repos/minigrep/minigrep/target/debug/deps/minigrep-a96225767c2cdeba.d: src/main.rs
-
-src/main.rs:
--- minigrep/target/debug/minigrep.d
+++
@@ -1,1 +0,0 @@
-/home/mhorrach/Repos/minigrep/minigrep/target/debug/minigrep: /home/mhorrach/Repos/minigrep/minigrep/src/main.rs
